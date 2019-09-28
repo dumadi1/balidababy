812c7f1f051e6d3346723c3265eb94b15ffc2a9c
dumadi1 <dumadi.lesi@gmail.com>
2019-09-28 14:19:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+var textArray = [
+    "",
+    "",
+    "",
+    "",""];
+  //  var images=['tryal.jpg','test.jpg','test2.jpg','pos.jpg'];
+    var back=['newbali.GIF','newbali2.GIF','giphy.gif','newbali3.GIF','newbali4.GIF']
+var count=-1;
+max=14;
+min=8;
+function makeDiv(){
+
+  count=count+1;
+  document.body.background=back[count];
+    var divsize = 3
+    //((Math.random()*(max - min + 1))).toFixed();
+    var color = '#'+ Math.round(0x000fff * Math.random()).toString(16);
+    $newdiv = $('<div/>').css({
+        'width':divsize+'em',
+        'height':divsize+'em',
+});
+    //var t=images[count];
+    //$newdiv.append('<img id="theImg" src="" />')
+    console.log(divsize)
+    var randomWord = textArray[count];
+    var posx = (Math.random()*(300 - 140 + 1));
+    var posy = (Math.random()*(300 - 140 + 1));
+//    console.log(posx);
+  //  console.log(posy);
+    $newdiv.text(randomWord).css({
+        'position':'absolute',
+        'left':posx+'px',
+        'top':posy+'px',
+        'display':'none',
+        'font-size':'3em',
+        'color' : color
+    }).appendTo( 'body' ).fadeIn(100).delay(5000).fadeOut(200, function(){
+      $(this).remove();
+       makeDiv();
+    });
+}
+
+
+makeDiv();
